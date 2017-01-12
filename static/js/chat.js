e92f204f5d08e8d83a388d8ec0d9bad5cce1e9a7
zxiaobai <664865665@qq.com>
2017-01-12 20:15:07 +0800

    no message

--- static/js/chat.js
+++ static/js/chat.js
@@ -75,6 +75,36 @@
         scrollToBottom(selector);
     };
 
+    // long time ago
+    var longTimeAgo = function() {
+      var timeAgo = function(time, ago) {
+        return Math.round(time) + ago;
+      };
+      $('time').each(function(i, e){
+        var past = parseInt(e.dataset.time);
+        var now = Math.round(new Date().getTime() / 1000);
+        var seconds = now - past;
+        var ago = seconds / 60;
+        // log('time ago', e, past, now, ago);
+        var oneHour = 60;
+        var oneDay = oneHour * 24;
+        // var oneWeek = oneDay * 7;
+        var oneMonth = oneDay * 30;
+        var oneYear = oneMonth * 12;
+        var s = '';
+        if(seconds < 60) {
+            s = timeAgo(seconds, ' 秒前')
+        } else if (ago < oneHour) {
+            s = timeAgo(ago, ' 分钟前');
+        } else if (ago < oneDay) {
+            s = timeAgo(ago/oneHour, ' 小时前');
+        } else if (ago < oneMonth) {
+            s = timeAgo(ago / oneDay, ' 天前');
+        }
+        $(e).text(s);
+      });
+    };
+
     var chatResponse = function(r) {
         var chat = JSON.parse(r);
         chatStore[chat.channel].push(chat);
@@ -124,16 +154,17 @@
           alertify.error('内容不能为空')
           return false
         }
+        $("#id-button-send").addClass("disabled");
         sendMessage();
         $('#id-input-content').val('');
         var count = 2;
         var countdown = setInterval(CountDown, 1000);
         function CountDown() {
-            $("#id-button-send").attr("disabled", true);
+            $("#id-button-send").addClass("disabled");
             $("#id-button-send").text("稍等"+count+"s");
             $("#id-button-send").css({'background':'#cccccc'})
             if (count == 0) {
-                $("#id-button-send").text("发送消息").removeAttr("disabled");
+                $("#id-button-send").text("发送消息").removeClass("disabled");
                 $("#id-button-send").css({'background':'#26a69a'})
                 clearInterval(countdown);
             }
@@ -156,39 +187,23 @@
       })
     };
 
-    // long time ago
-    var longTimeAgo = function() {
-      var timeAgo = function(time, ago) {
-        return Math.round(time) + ago;
-      };
-      $('time').each(function(i, e){
-        var past = parseInt(e.dataset.time);
-        var now = Math.round(new Date().getTime() / 1000);
-        var seconds = now - past;
-        var ago = seconds / 60;
-        // log('time ago', e, past, now, ago);
-        var oneHour = 60;
-        var oneDay = oneHour * 24;
-        // var oneWeek = oneDay * 7;
-        var oneMonth = oneDay * 30;
-        var oneYear = oneMonth * 12;
-        var s = '';
-        if(seconds < 60) {
-            s = timeAgo(seconds, ' 秒前')
-        } else if (ago < oneHour) {
-            s = timeAgo(ago, ' 分钟前');
-        } else if (ago < oneDay) {
-            s = timeAgo(ago/oneHour, ' 小时前');
-        } else if (ago < oneMonth) {
-            s = timeAgo(ago / oneDay, ' 天前');
-        }
-        $(e).text(s);
-      });
+
+    var sendMessageEnter = function () {
+        $(document).on('keyup',function(e){
+            if(e.keyCode === 13){
+                if($('#id-button-send').hasClass('disabled')){
+                    alertify.error('操作频繁')
+                }else {
+                    $('#id-button-send').click()
+                }
+            }
+        });
     };
 
     var __main = function(){
       subscribe();
       bindActions();
+      sendMessageEnter();
       // 选中第一个 channel 作为默认 channel
       $('.rc-channel')[0].click();
       // 更新时间的函数
